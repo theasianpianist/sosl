36d2757c8c75a5ea58428b88db09801c632efecc
Lawrence Lee <lfqlee314@gmail.com>
2017-05-01 22:16:10 -0400

    include exception name in search

--- src/tk/lawrencelee/StackOverflowSearchLib.java
+++ src/tk/lawrencelee/StackOverflowSearchLib.java
@@ -11,28 +11,26 @@ import java.net.URISyntaxException;
 
 public class StackOverflowSearchLib {
     public static void search(Exception e) {
+        String exceptionName = e.getClass().toString();
         String message = e.getMessage().replace(" ", "+");
+        String query = "";
+        if (exceptionName.equals("java.lang.IndexOutOfBoundsException")) {
+            query += exceptionName;
+        }
+        else {
+            query += exceptionName + message;
+        }
         String stackOverflow = "http://stackoverflow.com/search?q=";
         String google = "https://www.google.com/#q=";
         try {
-            Desktop.getDesktop().browse(new URI(stackOverflow + message));
-            Desktop.getDesktop().browse(new URI(google + message));
+            Desktop.getDesktop().browse(new URI(stackOverflow + query));
+            Desktop.getDesktop().browse(new URI(google + query));
         }
         catch (URISyntaxException ex) {
             System.exit(1);
         }
         catch (IOException ex) {
             System.exit(1);
-        }
-    }
-
-    public static void main (String[] args) {
-        Exception e = new IllegalAccessException("test message");
-        try {
-            throw e;
-        }
-        catch (Exception ex) {
-            search(ex);
         }
     }
 }
