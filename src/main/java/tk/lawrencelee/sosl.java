d1e1b412af90b3ce5949c2224aa2fbc189f0f2f0
Lawrence Lee <lfqlee314@gmail.com>
2017-05-03 15:02:34 -0400

    fix things

--- src/main/java/tk/lawrencelee/sosl.java
+++ src/main/java/tk/lawrencelee/sosl.java
@@ -6,16 +6,26 @@ package tk.lawrencelee;
 
 import java.awt.Desktop;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 public class sosl implements Thread.UncaughtExceptionHandler{
+    ArrayList<String> specialExceptions = new ArrayList<String>();
     public void uncaughtException(Thread t, Throwable e) {
-        e.printStackTrace();
+        specialExceptions.add("java.lang.IndexOutOfBoundsException");
         String exceptionName = e.getClass().toString().split(" ")[1];
         String message = e.getMessage().replace(" ", "+");
+        try {
+            message = URLEncoder.encode(message, "UTF-8");
+        }
+        catch (UnsupportedEncodingException ex) {
+            ex.printStackTrace();
+        }
         String query = "";
-        if (exceptionName.equals("java.lang.IndexOutOfBoundsException")) {
+        if (specialExceptions.contains(exceptionName)) {
             query += exceptionName;
         }
         else {
@@ -23,18 +33,31 @@ public class sosl implements Thread.UncaughtExceptionHandler{
         }
         String stackOverflow = "http://stackoverflow.com/search?q=";
         String google = "https://www.google.com/#q=";
+        String searchTerm = stackOverflow + query;
         try {
             Desktop.getDesktop().browse(new URI(stackOverflow + query));
             Desktop.getDesktop().browse(new URI(google + query));
+            e.printStackTrace();
         }
         catch (UnsupportedOperationException ex) {
             System.out.println("Unfortunately your system does not support this library");
+            try {
+                t.sleep(100);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
         }
         catch (URISyntaxException ex) {
-            e.printStackTrace();
+            ex.printStackTrace();
         }
         catch (IOException ex) {
-            e.printStackTrace();
+            ex.printStackTrace();
         }
+    }
+
+    public static void main (String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler(new sosl());
+        int i = Integer.parseInt("oetn");
     }
 }
